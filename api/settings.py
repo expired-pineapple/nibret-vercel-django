64d4c2dbfe0ba4eb63eb45a53a105097d2ea7192
expired-pineapple <ruthtirusew944@gmail.com>
2024-12-16 17:38:55 +0300

    debug enabled

--- api/settings.py
+++ api/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
