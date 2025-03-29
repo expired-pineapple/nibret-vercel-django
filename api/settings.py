e4c3ae150d0d3066306803481e623c571f1a35e1
Ruth Tirusew <87856799+expired-pineapple@users.noreply.github.com>
2025-03-29 11:40:57 +0300

    add nibret.com on allowed hosts

--- api/settings.py
+++ api/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','localhost', 'api.nibret.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
