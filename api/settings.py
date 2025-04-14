be46c028306c355d694e49e95ca555677b06650c
Ruth Tirusew <87856799+ruth-tirusew@users.noreply.github.com>
2025-04-14 17:22:02 +0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -116,8 +116,6 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
-ACCOUNT_EMAIL_REQUIRED = False
-ACCOUNT_EMAIL_VERIFICATION = "none" 
 AUTH_USER_MODEL = 'authentication.UserAccount'
 ACCOUNT_ADAPTER = 'authentication.adapter.CustomAccountAdapter'
 
